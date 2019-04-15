be54098204ce10be3239c4abe616fe5a1c38f0a2
Akshat Khandelwal <ak127a@gmail.com>
2019-04-15 20:00:31 +0530

    Chnaged styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,17 +3,29 @@ var jsontext = document.getElementById("json");
 var tablesection = document.getElementById("tableSection");
 var foundIndex = null;
 
-window.onload = function() {
-  header.style.transform = "translate3d(0, 0, 0)";
-  tablesection.style.display = "none";
-};
+document.getElementById("main-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  console.log("Submit");
+  updateTable();
+});
 
-function foo() {
+// TESTING
+
+function updateTable() {
+  console.log("Documnet loaded");
+
+  // newRow += "<div class='table-element'>Name</div>";
+  // newRow += "<div class='table-element'>" + 1 + "</div>";
+  // newRow += "<div class='table-element'>" + 2 + "</div>";
+  // newRow += "</div>";
+}
+
+function updateTable() {
   var enteredName = document.getElementById("name").value;
   var found = false;
-  // tablesection.style.display = "block";
+  var tableContainer = document.getElementsByClassName("table-container");
 
-  for (var i = 3; i <= 6; i++) {
+  for (var i = 3; i <= 7; i++) {
     if (resultArray[i][1] == enteredName) {
       found = true;
       foundIndex = i;
@@ -21,29 +33,74 @@ function foo() {
     }
   }
 
+  // TESTING ---- replace found by true
   if (found) {
-    var t = "";
-    // First ROW
-    t += "<tr>";
-    t += "<th>USN</th>";
-    t += "<th>Name</th>";
-    // Last Date
-    t += "<th>" + resultArray[1][resultArray[1].length - 21] + "</th>";
-    // Last second date
-    t += "<th>" + resultArray[1][resultArray[1].length - 22] + "</th>";
-    t += "</tr>";
-    // Second ROW
-    var tr = "<tr>";
-    tr += "<td>" + resultArray[foundIndex][0] + "</td>";
-    tr += "<td>" + resultArray[foundIndex][1] + "</td>";
-    tr += "<td>" + resultArray[foundIndex][2] + "</td>";
-    tr += "<td>" + resultArray[foundIndex][3] + "</td>";
-    tr += "</tr>";
-    t += tr;
-    document.getElementById("dataTable").innerHTML += t;
-    tablesection.style.display = "block";
+    var newElement;
+    var newRow;
+
+    console.log(tableContainer);
+
+    newRow = document.createElement("div");
+    newRow.className = "table-row";
+    var itemname = null;
+    for (var i = 0; i < 4; i++) {
+      newElement = document.createElement("div");
+      newElement.className = "table-element";
+      switch (i) {
+        case 0:
+          itemname = "USN";
+          break;
+        case 1:
+          itemname = "Name";
+          break;
+        case 2:
+          itemname = resultArray[1][resultArray[1].length - 21];
+          break;
+        case 3:
+          itemname = resultArray[1][resultArray[1].length - 22];
+          break;
+      }
+
+      newElement.innerHTML = i;
+      newRow.appendChild(newElement);
+      tableContainer[0].appendChild(newRow);
+    }
+    for (var i = 0; i < 4; i++) {
+      newElement = document.createElement("div");
+      newElement.className = "table-element";
+      newElement.innerHTML = resultArray[foundIndex][i];
+      newRow.appendChild(newElement);
+      tableContainer[0].appendChild(newRow);
+    }
   } else {
-    tablesection.innerHTML += "Name not found";
-    tablesection.style.display = "block";
+    tableContainer.innerHTML = "Item Not Found";
   }
+
+  tablesection.style.display = "block";
+
+  // if (found) {
+  //   var t = "";
+  //   // First ROW
+  //   t += "<tr>";
+  //   t += "<th>USN</th>";
+  //   t += "<th>Name</th>";
+  //   // Last Date
+  //   t += "<th>" + resultArray[1][resultArray[1].length - 21] + "</th>";
+  //   // Last second date
+  //   t += "<th>" + resultArray[1][resultArray[1].length - 22] + "</th>";
+  //   t += "</tr>";
+  //   // Second ROW
+  //   var tr = "<tr>";
+  //   tr += "<td>" + resultArray[foundIndex][0] + "</td>";
+  //   tr += "<td>" + resultArray[foundIndex][1] + "</td>";
+  //   tr += "<td>" + resultArray[foundIndex][2] + "</td>";
+  //   tr += "<td>" + resultArray[foundIndex][3] + "</td>";
+  //   tr += "</tr>";
+  //   t += tr;
+  //   document.getElementById("dataTable").innerHTML += t;
+  //   tablesection.style.display = "block";
+  // } else {
+  //   tablesection.innerHTML += "Name not found";
+  //   tablesection.style.display = "block";
+  // }
 }
